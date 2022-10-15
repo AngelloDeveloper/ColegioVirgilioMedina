e2d22a6c6d38eb8297cb22b410cac48ab704bb07
AngelloDuran <aduran@travelsupport.info>
2022-10-15 11:47:37 -0400

    estilizacion login

--- auth/login.php
+++ auth/login.php
@@ -1,8 +1,73 @@
-<?php require('../utils/header.php'); ?>
-	
-	<!--Section-->
-	<section>
-		
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--     Fonts and icons     -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  	<!-- CSS Files -->
+  	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="css/login.css" />
+	<title>Colegio Virgilio Medina</title>
+</head>
+<body>
+	<header class="mb-4">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4">
+					<img class="logo" src="../assets/img/escudopng2.png" />
+				</div>
+				<div class="col-md-8"></div>
+			</div>
+		</div>
+	</header>
+	<section class="mt-4">
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="card">
+					<div class="card-body">
+						<div class="row">
+							<div class="col-12">
+								<center>
+									<img width="200" src="../assets/img/escudopng2.png" />
+								</center>
+							</div>
+						</div>
+						<hr />
+						<div class="row">
+							<div class="col-12">
+								<form>
+									<div class="form-group">
+										<label for="email">CorreoElectrónico</label>
+										<input type="email" name="email" id="email" class="form-control input" required />
+									</div>
+									<div class="form-group">
+										<label for="password">Clave</label>
+										<input type="password" name="password" id="password" class="form-control input" required />
+									</div>
+									<div class="form-group">
+										<button class="btn">Ingresar</button>
+									</div>
+								</form>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4"></div>
+		</div>
 	</section>
-
-<?php require('../utils/footer.php'); ?>
+	<footer>
+		
+	</footer>
+	<!--   Core JS Files   -->
+	<script src="../assets/js/core/jquery.min.js"></script>
+	<script src="../assets/js/core/popper.min.js"></script>
+	<script src="../assets/js/core/bootstrap.min.js"></script>
+	<script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+	<script type="text/javascript" src="js/login.js"></script>
+</body>
+</html>
