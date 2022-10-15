2356ffba14193f401689ef0c4b997b9bbff7f423
AngelloDuran <aduran@travelsupport.info>
2022-10-15 12:24:35 -0400

    login

--- auth/login.php
+++ auth/login.php
@@ -23,42 +23,48 @@
 		</div>
 	</header>
 	<section class="mt-4">
-		<div class="row">
-			<div class="col-md-4"></div>
-			<div class="col-md-4">
-				<div class="card">
-					<div class="card-body">
-						<div class="row">
-							<div class="col-12">
-								<center>
-									<img width="200" src="../assets/img/escudopng2.png" />
-								</center>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4">
+					
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-12">
+									<center>
+										<img width="200" src="../assets/img/escudopng2.png" />
+									</center>
+								</div>
+							</div>
+							<hr />
+							<div class="row">
+								<div class="col-12">
+									<form>
+										<div class="form-group">
+											<label for="email">CorreoElectrónico</label>
+											<input type="email" name="email" id="email" class="form-control input" required />
+										</div>
+										<div class="form-group">
+											<label for="password">Clave</label>
+											<input type="password" name="password" id="password" class="form-control input" required />
+										</div>
+										<div class="form-group">
+											<button class="btn">Ingresar</button>
+										</div>
+									</form>
+								</div>
 							</div>
 						</div>
-						<hr />
-						<div class="row">
-							<div class="col-12">
-								<form>
-									<div class="form-group">
-										<label for="email">CorreoElectrónico</label>
-										<input type="email" name="email" id="email" class="form-control input" required />
-									</div>
-									<div class="form-group">
-										<label for="password">Clave</label>
-										<input type="password" name="password" id="password" class="form-control input" required />
-									</div>
-									<div class="form-group">
-										<button class="btn">Ingresar</button>
-									</div>
-								</form>
-							</div>
-						</div>
-						
 					</div>
 				</div>
+				<div class="col-md-4">
+					
+				</div>
 			</div>
-			<div class="col-md-4"></div>
 		</div>
+		
 	</section>
 	<footer>
 		
@@ -71,3 +77,5 @@
 	<script type="text/javascript" src="js/login.js"></script>
 </body>
 </html>
+
+<!---->
